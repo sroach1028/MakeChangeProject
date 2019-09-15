5164a67fcbce10fc0e2f83a65d2926f5bde28451
Shaun Roach <sroach1028@gmail.com>
2019-09-14 20:02:30 -0600

    added more methods. this is complete and works great

--- src/MakeChangeApp.java
+++ src/MakeChangeApp.java
@@ -5,38 +5,70 @@ public class MakeChangeApp {
 	public static void main(String[] args) {
 		Scanner kb = new Scanner(System.in);
 		
-		double totalCost, amountTendered;
-		
-		System.out.print("Total Cost: ");
-		totalCost = kb.nextDouble();
-		System.out.println("Amount tendered: ");
-		amountTendered = kb.nextDouble();
+		double totalCost = getCost(kb); 
+		double amountTendered = getAmountTendered(kb);
 		double amountOwed = amountTendered - totalCost;
-		if (amountTendered < amountOwed) {
-			amountTendered = errorMessage(kb);
+		
+		if (amountTendered == totalCost) {
+			exactMessaage();
+		}
+		else if (amountTendered < totalCost) {
+			errorMessage(kb);
+		}
+		else {
+		makeChange(amountOwed);
 		}
 		
-		int dollarsBack = (int) Math.floor(amountOwed);
-		double centsBack = Math.round((amountOwed - dollarsBack)*100);
-		
-		System.out.println("Your change is: ");
-		if (dollarsBack >0) {
-			getDollarsOwed(dollarsBack);
-		}
-		
-		if (centsBack >0 ) {
-			getCentsOwed(centsBack);
-		}
-
 		kb.close();
-	}
+	}  //end of main
 		
 
 	
 	//////******************************METHODS******************************************/////
 	
-	public static void getDollarsOwed(double dollars) {
+	public static double getCost(Scanner kb) {            // gets total cost from the user as input
+		System.out.print("Total Cost: ");
+		return kb.nextDouble();
+	} // end getCost
+	//**************************************************************************************************************//
+	
+	public static double getAmountTendered(Scanner kb) {  //gets amount tendered from user as input
+		System.out.print("Amount Tendered: ");
+		return kb.nextDouble();
+	} // end getAmountTendered
+	//*************************************************************************************************************//
+	
+	public static void exactMessaage() {      //prints message if amount tendered exactly matches total cost
+		System.out.println("Wow, that's the exact amount owed. You should know, starting today, I also accept tips, although it's against company policy. "
+				+ "			\n...But I already gave my two weeks so it's fine. My boss has been riding me and I just can't take the politics. "
+				+ "			\nCan you believe I had to come in on Christmas? Then in April they cut my hours to part-time and stopped offering stock options. So yeah, tips are great. "
+				+ "			\n...What? Oh, yes, you are free to go. Take me with you?");
+	} // end exactMessage
+	//*************************************************************************************************************//
+	
+	public static void errorMessage(Scanner kb) { 				// prints error message if amount tendered is less than the total cost
+		System.out.println("That's not enough. Come back when you have more money. ");
+	} // end errorMessage
+	//*************************************************************************************************************//
+	
+	public static void makeChange(double amountOwed) { 		// takes total amount owed to the user, separates dollars and cents and passes them to getDollarsOwed and getCentsOwed
+		int dollarsBack = (int) Math.floor(amountOwed);
+		int centsBack = (int) Math.round((amountOwed - dollarsBack)*100);
+		
+		System.out.println("Your change is: ");
+		
+		if (dollarsBack >0) {
+			getDollarsOwed(dollarsBack);
+		}
+		if (centsBack >0 ) {
+			getCentsOwed(centsBack);
+		}
+	} // end makeChange
+	//*************************************************************************************************************//
+	
+	public static void getDollarsOwed(double dollars) {  		// takes just the dollar amount from makeChange, figures amount of each denomination due back to the user and passes to printMethod
 		int twenties = 0, tens = 0, fives = 0, ones = 0; 
+		
 		do {
 			if (dollars%5 !=0) {
 				ones++;
@@ -62,13 +94,16 @@ public class MakeChangeApp {
 			}
 			
 		} while (dollars>0);
+		
 		printMethod(twenties, "Twenty", "Twenties");
 		printMethod(tens, "Ten", "Tens");
 		printMethod(fives, "Five", "Fives");
-		printMethod(ones, "One", "Ones");
-	}
+		printMethod(ones, "One Dollar Bill", "Ones");
 	
-	public static void getCentsOwed(double cents) {
+	} // end getDollarsOwed
+	//*************************************************************************************************************//
+	
+	public static void getCentsOwed(double cents) {				// takes just the cents from makeChange, figures the amount of each denomination due back to the user, and passes to printMethod
 		int pennies = 0, nickles = 0, dimes = 0, quarters = 0;
 		
 		do {
@@ -101,8 +136,10 @@ public class MakeChangeApp {
 		printMethod(nickles, "Nickle", "Nickles");
 		printMethod(pennies, "Penny", "Pennies");
 		
-	}
-	public static void printMethod(int amountOfEach, String single, String multiple) {
+	}//end getCentsOwed
+	//*************************************************************************************************************//
+	
+	public static void printMethod(int amountOfEach, String single, String multiple) {		// takes amounts from getDollarsOwed and getCentsOwed, and outputs each
 		switch(amountOfEach) {
 		case 0:
 			break;
@@ -110,15 +147,11 @@ public class MakeChangeApp {
 			break;
 		default: System.out.println(amountOfEach + " " + multiple);
 		}
-	}
-	public static double errorMessage(Scanner kb) {
-		System.out.println("That's not enough money. Give me more. ");
-		System.out.println("Amount tendered: ");
-		return kb.nextDouble();
-	}
-	
-	
-}
+	} // end printMethod
+
+//*****************************End Methods***********************//	
+
+} //end MakeChangeApp Class
 
 
 
